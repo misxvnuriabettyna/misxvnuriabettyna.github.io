3c7bd6d58d78488feef65bf8d5cd258a02301c61
deleitempanadas <deleite.empanadas2012@gmail.com>
2024-08-06 13:52:28 -0600

    first commit

---
+++ js/demo003.js
@@ -0,0 +1,821 @@
+window.sr = ScrollReveal();
+
+		sr.reveal('.ornamentainicio', {
+		duration: 2000, //tiempo
+		//delay:1500,
+		origin: 'top', //origen
+		distance: '40px' //distancia
+
+
+	}); 
+
+	sr.reveal('.nombres1', {
+		duration: 2500, //tiempo
+		//delay:1500,
+		origin: 'bottom', //origen
+		distance: '40px' //distancia
+
+
+	}); 
+	sr.reveal('.nombres2', {
+		duration: 2500, //tiempo
+		//delay:1500,
+		origin: 'top', //origen
+		distance: '40px' //distancia
+
+
+	}); 
+		sr.reveal('#padres2', {
+		duration: 2500, //tiempo
+		delay:500,
+		origin: 'left', //origen
+		distance: '50px' //distancia
+
+
+	}); 
+	sr.reveal('#padres', {
+		duration: 2500, //tiempo
+		delay:500,
+		origin: 'top', //origen
+		distance: '40px' //distancia
+
+
+	}); 
+	sr.reveal('#nombresPadres', {
+		duration: 1500, //tiempo
+		delay:700,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+	sr.reveal('.foto', {
+		rotate: {
+        x: 100,
+        z: 100
+    },
+		duration: 2000, //tiempo
+		//delay:1500,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	});
+	sr.reveal('.iniciales2', {
+		rotate: {
+        x: 100,
+        z: 100
+    },
+		duration: 2500, //tiempo
+		delay:3000,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	});
+	sr.reveal('.vestimenta', {
+		rotate: {
+        x: 0,
+        z: 50
+    },
+		duration: 2500, //tiempo
+		delay:500,
+		origin: 'top', //origen
+		distance: '50px' //distancia
+
+
+	});
+	sr.reveal('.lluvia', {
+		rotate: {
+        x: 0,
+        z: 50
+    },
+		duration: 2500, //tiempo
+		delay:500,
+		origin: 'top', //origen
+		distance: '50px' //distancia
+
+
+	});
+	
+	sr.reveal('.textodondeycuando', {
+		duration: 2200, //tiempo
+		//delay:1500,
+		origin: 'bottom', //origen
+		distance: '40px' //distancia
+
+
+	});
+	sr.reveal('.respetuosamente', {
+		duration: 2500, //tiempo
+		delay:500,
+		origin: 'bottom', //origen
+		distance: '50px' //distancia
+
+
+	});  
+	sr.reveal('.tespero', {
+		duration: 2500, //tiempo
+		delay:500,
+		origin: 'bottom', //origen
+		distance: '50px' //distancia
+
+
+	});  
+	sr.reveal('#salonIglesia', {
+		duration: 2500, //tiempo
+		delay:500,
+		origin: 'bottom', //origen
+		distance: '50px' //distancia
+
+
+	});  
+	sr.reveal('.lugar', {
+		duration: 2500, //tiempo
+		delay:500,
+		origin: 'bottom', //origen
+		distance: '50px' //distancia
+
+
+	}); 
+		sr.reveal('#location', {
+		duration: 2500, //tiempo
+		delay:500,
+		origin: 'top', //origen
+		distance: '50px' //distancia
+
+
+	}); 
+			sr.reveal('.hora', {
+		duration: 2500, //tiempo
+		delay:500,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+			sr.reveal('#icono', {
+		duration: 2000, //tiempo
+		delay:700,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+			
+sr.reveal('.invitacionpara', {
+		duration: 2500, //tiempo
+		delay:500,
+		origin: 'right', //origen
+		distance: '50px' //distancia
+
+
+	});	
+	sr.reveal('#mesa', {
+		duration: 2000, //tiempo
+		//delay:1500,
+		origin: 'right', //origen
+		distance: '50px' //distancia
+
+
+	});				
+sr.reveal('#textolibre', {
+		duration: 2500, //tiempo
+		delay:500,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+
+	sr.reveal('.textoNosCasamos', {
+		duration: 2000, //tiempo
+		//delay:1500,
+		origin: 'top', //origen
+		distance: '200px', //distancia
+		
+
+	});
+		sr.reveal('.dia', {
+		duration: 2700, //tiempo
+		//delay:1500,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+		sr.reveal('.mes', {
+		duration: 2000, //tiempo
+		delay:900,
+		origin: 'right', //origen
+		distance: '100px' //distancia
+
+
+	}); 
+		sr.reveal('.mes2', {
+		duration: 2000, //tiempo
+		delay:1700,
+		origin: 'left', //origen
+		distance: '100px' //distancia
+
+
+	}); 
+		sr.reveal('.año', {
+	
+
+
+	}); 
+			sr.reveal('#cuenta', {
+		duration: 2500, //tiempo
+		delay:700,
+		origin: 'bottom', //origen
+		distance: '100px', //distancia
+		
+
+	});
+				sr.reveal('.itinerario', {
+		duration: 2500, //tiempo
+		delay:700,
+		origin: 'bottom', //origen
+		distance: '200px', //distancia
+		
+
+	});
+						sr.reveal('.evitarDress', {
+		duration: 2500, //tiempo
+		delay:700,
+		origin: 'bottom', //origen
+		distance: '200px', //distancia
+		
+
+	});
+		sr.reveal('.tesperoTexto', {
+		duration: 2500, //tiempo
+		delay:700,
+		origin: 'bottom', //origen
+		distance: '200px', //distancia
+		
+
+	});
+	sr.reveal('.ceremoniaReligiosa', {
+		duration: 1500, //tiempo
+		///delay:1000,
+		origin: 'top', //origen
+		scale: 0.30,
+		distance: '-0' //distancia
+		
+
+	});
+	sr.reveal('.mapas', {
+		duration: 2000, //tiempo
+		//delay:100,
+		origin: 'top', //origen
+		distance: '200px', //distancia
+		
+
+	});
+sr.reveal('.btnComoLlegar', {
+		duration: 2500, //tiempo
+		delay:800,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+
+	});
+sr.reveal('.btnWhatsNovios', {
+		duration: 2500, //tiempo
+		delay:1000,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+
+	});
+sr.reveal('.textoasistencia', {
+		duration: 2500, //tiempo
+		delay:500,
+		origin: 'top', //origen
+		distance: '50px', //distancia
+		
+
+	});
+	sr.reveal('#numero', {
+		duration: 2500, //tiempo
+		delay:500,
+		origin: 'left', //origen
+		distance: '50px', //distancia
+		
+
+	});
+	sr.reveal('#imagen1', {
+		duration: 2500, //tiempo
+		//delay:1500,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+
+	});
+	sr.reveal('.collage', {
+		duration: 2500, //tiempo
+		///delay:1000,
+		origin: 'top', //origen
+		scale: 0.50,
+		distance: '-0' //distancia
+		
+
+	});
+		sr.reveal('#imagen3', {
+		duration: 2500, //tiempo
+		//delay:1500,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+		
+
+	});
+	sr.reveal('.familiaboleto', {
+		duration: 1500, //tiempo
+		delay:100,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+			sr.reveal('.apellidos', {
+		duration: 1500, //tiempo
+		//delay:700,
+		origin: 'bottom', //origen
+		distance: '90px' //distancia
+
+
+	}); 
+				sr.reveal('.invitacionpara', {
+		duration: 2500, //tiempo
+		delay:500,
+		origin: 'left', //origen
+		distance: '50px' //distancia
+
+
+	}); 
+	sr.reveal('#amazonliverpool', {
+		duration: 1500, //tiempo
+		//delay:700,
+		origin: 'bottom', //origen
+		distance: '90px' //distancia
+
+
+	}); 
+			sr.reveal('.form-register', {
+		duration: 1000, //tiempo
+		//delay:800,
+		origin: 'top', //origen
+		distance: '300px', //distancia
+		
+
+	});
+			sr.reveal('.btnWhats', {
+		duration: 1500, //tiempo
+		//delay:700,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+sr.reveal('.nosCasamos', {
+		duration: 1500, //tiempo
+		//delay:1500,
+		origin: 'top', //origen
+		distance: '-100px', //distancia
+		
+
+	});
+
+sr.reveal('.dondeyCuando', {
+		duration: 2500, //tiempo
+		delay:500,
+		origin: 'bottom', //origen
+		distance: '30px', //distancia
+		
+
+	});
+sr.reveal('.dondeyCuando2', {
+		duration: 2500, //tiempo
+		delay:500,
+		origin: 'top', //origen
+		distance: '30px', //distancia
+		
+
+	});
+
+sr.reveal('.carousel', {
+		duration: 2000, //tiempo
+		//delay:1500,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+sr.reveal('.Tituloasistencia', {
+		duration: 1500, //tiempo
+		origin: 'top', //origen
+		distance: '-100px', //distancia
+		reset:1000
+
+	});
+sr.reveal('.Tituloasistenciaespero', {
+		duration: 1500, //tiempo
+		origin: 'top', //origen
+		distance: '-100px', //distancia
+		reset:1000
+
+	});
+
+sr.reveal('.tiendas', {
+		duration: 1500, //tiempo
+		//delay:700,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+
+
+
+sr.reveal('.textilibre', {
+		duration: 1500, //tiempo
+		origin: 'left', //origen
+		distance: '-100px', //distancia
+		reset:1000
+
+	});
+	sr.reveal('.piepagina', {
+		duration: 1500, //tiempo
+		origin: 'bottom', //origen
+		distance: '-100px', //distancia
+		reset:1000
+
+	}); 
+
+
+	sr.reveal('.table-responsive', {
+		duration: 1500, //tiempo
+		origin: 'bottom', //origen
+		distance: '-100px', //distancia
+		
+
+	});
+	sr.reveal('.linea', {
+		duration: 700, //tiempo
+		///delay:1000,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+		
+
+	});
+	///////////////////////////////////////////////////////////////////////////////////////////7
+	// Initialize and add the map
+const playpause = document.querySelector('.playpause');
+
+playpause.addEventListener('click', () => {
+  playpause.classList.toggle('playing');
+});
+
+function PlayAudio() {
+       var audio = document.getElementById("musica1");
+       if (audio.paused) {
+          audio.play(); 
+        
+       } else {
+          audio.pause();
+         
+       }
+    }
+var x, i, j, l, ll, selElmnt, a, b, c;
+/* Look for any elements with the class "custom-select": */
+x = document.getElementsByClassName("custom-select");
+l = x.length;
+for (i = 0; i < l; i++) {
+  selElmnt = x[i].getElementsByTagName("select")[0];
+  ll = selElmnt.length;
+  /* For each element, create a new DIV that will act as the selected item: */
+  a = document.createElement("DIV");
+  a.setAttribute("class", "select-selected");
+  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+  x[i].appendChild(a);
+  /* For each element, create a new DIV that will contain the option list: */
+  b = document.createElement("DIV");
+  b.setAttribute("class", "select-items select-hide");
+  for (j = 1; j < ll; j++) {
+    /* For each option in the original select element,
+    create a new DIV that will act as an option item: */
+    c = document.createElement("DIV");
+    c.innerHTML = selElmnt.options[j].innerHTML;
+    c.addEventListener("click", function(e) {
+        /* When an item is clicked, update the original select box,
+        and the selected item: */
+        var y, i, k, s, h, sl, yl;
+        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+        sl = s.length;
+        h = this.parentNode.previousSibling;
+        for (i = 0; i < sl; i++) {
+          if (s.options[i].innerHTML == this.innerHTML) {
+            s.selectedIndex = i;
+            h.innerHTML = this.innerHTML;
+            y = this.parentNode.getElementsByClassName("same-as-selected");
+            yl = y.length;
+            for (k = 0; k < yl; k++) {
+              y[k].removeAttribute("class");
+            }
+            this.setAttribute("class", "same-as-selected");
+            break;
+          }
+        }
+        h.click();
+    });
+    b.appendChild(c);
+  }
+  x[i].appendChild(b);
+  a.addEventListener("click", function(e) {
+    /* When the select box is clicked, close any other select boxes,
+    and open/close the current select box: */
+    e.stopPropagation();
+    closeAllSelect(this);
+    this.nextSibling.classList.toggle("select-hide");
+    this.classList.toggle("select-arrow-active");
+  });
+}
+
+function closeAllSelect(elmnt) {
+  /* A function that will close all select boxes in the document,
+  except the current select box: */
+  var x, y, i, xl, yl, arrNo = [];
+  x = document.getElementsByClassName("select-items");
+  y = document.getElementsByClassName("select-selected");
+  xl = x.length;
+  yl = y.length;
+  for (i = 0; i < yl; i++) {
+    if (elmnt == y[i]) {
+      arrNo.push(i)
+    } else {
+      //y[i].classList.remove("select-arrow-active");
+    }
+  }
+  for (i = 0; i < xl; i++) {
+    if (arrNo.indexOf(i)) {
+      x[i].classList.add("select-hide");
+    }
+  }
+}
+
+
+// Ejemplo de JavaScript inicial para deshabilitar el envío de formularios si hay campos no válidos
+(function () {
+  'use strict'
+
+  // Obtener todos los formularios a los que queremos aplicar estilos de validación de Bootstrap personalizados
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Bucle sobre ellos y evitar el envío
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+
+$(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#scroll').fadeIn(); 
+        } else { 
+            $('#scroll').fadeOut(); 
+        } 
+    }); 
+    $('#scroll').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
+});
+
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+
+
+function HerreraRobles() {
+    window.open("pases/HerreraRobles.html", "_self");        
+}
+function BautistaHerrera() {
+    window.open("pases/BautistaHerrera.html", "_self");        
+}
+function personas3() {
+    window.open("pases/inicio3.html", "_self");        
+}
+function personas4() {
+    window.open("pases/inicio4.html", "_self");        
+}
+function personas5() {
+    window.open("pases/inicio5.html", "_self");        
+}
+function personas6() {
+    window.open("pases/inicio6.html", "_self");        
+}
+function personas7() {
+    window.open("pases/inicio7.html", "_self");        
+}
+function personas8() {
+    window.open("pases/inicio8.html", "_self");        
+}
+function personas9() {
+    window.open("pases/inicio9.html", "_self");        
+}
+
+ function copiarBarronGloria(){
+  var origen = document.getElementById('target1');
+  var destino = document.getElementById('target10');
+  var copyFrom = document.createElement("textarea");
+  copyFrom.textContent = origen.value;
+  var body = document.getElementsByTagName('body')[0];
+  body.appendChild(copyFrom);
+  copyFrom.select();
+  document.execCommand('copy');
+  alert("Copiado al portapapeles!");
+  body.removeChild(copyFrom);
+  destino.focus();
+  document.execCommand('paste');
+}
+ function copiarBautistaHerrera(){
+  var origen = document.getElementById('target2');
+  var destino = document.getElementById('target10');
+  var copyFrom = document.createElement("textarea");
+  copyFrom.textContent = origen.value;
+  var body = document.getElementsByTagName('body')[0];
+  body.appendChild(copyFrom);
+  copyFrom.select();
+  document.execCommand('copy');
+  alert("Copiado al portapapeles!");
+  body.removeChild(copyFrom);
+  destino.focus();
+  document.execCommand('paste');
+}
+ function copiar3(){
+  var origen = document.getElementById('target3');
+  var destino = document.getElementById('target10');
+  var copyFrom = document.createElement("textarea");
+  copyFrom.textContent = origen.value;
+  var body = document.getElementsByTagName('body')[0];
+  body.appendChild(copyFrom);
+  copyFrom.select();
+  document.execCommand('copy');
+  alert("Copiado al portapapeles!");
+  body.removeChild(copyFrom);
+  destino.focus();
+  document.execCommand('paste');
+}
+ function copiar4(){
+  var origen = document.getElementById('target4');
+  var destino = document.getElementById('target10');
+  var copyFrom = document.createElement("textarea");
+  copyFrom.textContent = origen.value;
+  var body = document.getElementsByTagName('body')[0];
+  body.appendChild(copyFrom);
+  copyFrom.select();
+  document.execCommand('copy');
+  alert("Copiado al portapapeles!");
+  body.removeChild(copyFrom);
+  destino.focus();
+  document.execCommand('paste');
+}
+ function copiar5(){
+  var origen = document.getElementById('target5');
+  var destino = document.getElementById('target10');
+  var copyFrom = document.createElement("textarea");
+  copyFrom.textContent = origen.value;
+  var body = document.getElementsByTagName('body')[0];
+  body.appendChild(copyFrom);
+  copyFrom.select();
+  document.execCommand('copy');
+  alert("Copiado al portapapeles!");
+  body.removeChild(copyFrom);
+  destino.focus();
+  document.execCommand('paste');
+}
+ function copiar6(){
+  var origen = document.getElementById('target6');
+  var destino = document.getElementById('target10');
+  var copyFrom = document.createElement("textarea");
+  copyFrom.textContent = origen.value;
+  var body = document.getElementsByTagName('body')[0];
+  body.appendChild(copyFrom);
+  copyFrom.select();
+  document.execCommand('copy');
+  alert("Copiado al portapapeles!");
+  body.removeChild(copyFrom);
+  destino.focus();
+  document.execCommand('paste');
+}
+ function copiar7(){
+  var origen = document.getElementById('target7');
+  var destino = document.getElementById('target10');
+  var copyFrom = document.createElement("textarea");
+  copyFrom.textContent = origen.value;
+  var body = document.getElementsByTagName('body')[0];
+  body.appendChild(copyFrom);
+  copyFrom.select();
+  document.execCommand('copy');
+  alert("Copiado al portapapeles!");
+  body.removeChild(copyFrom);
+  destino.focus();
+  document.execCommand('paste');
+}
+ function copiar8(){
+  var origen = document.getElementById('target8');
+  var destino = document.getElementById('target10');
+  var copyFrom = document.createElement("textarea");
+  copyFrom.textContent = origen.value;
+  var body = document.getElementsByTagName('body')[0];
+  body.appendChild(copyFrom);
+  copyFrom.select();
+  document.execCommand('copy');
+  alert("Copiado al portapapeles!");
+  body.removeChild(copyFrom);
+  destino.focus();
+  document.execCommand('paste');
+}
+ function copiar9(){
+  var origen = document.getElementById('target9');
+  var destino = document.getElementById('target10');
+  var copyFrom = document.createElement("textarea");
+  copyFrom.textContent = origen.value;
+  var body = document.getElementsByTagName('body')[0];
+  body.appendChild(copyFrom);
+  copyFrom.select();
+  document.execCommand('copy');
+  alert("Copiado al portapapeles!");
+  body.removeChild(copyFrom);
+  destino.focus();
+  document.execCommand('paste');
+}
